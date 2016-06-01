a7e0fce4232b5ec0fd6a6942c41242a355a284f7
yida <1143922395@qq.com>
2016-06-02 00:23:25 +0800

    增加了新功能，可以改变画笔大小和颜色

--- flask/static/new.js
+++ flask/static/new.js
@@ -6,19 +6,19 @@ function Draw(wrap) {
     this.wrap = $(wrap);
     this.ctx = this.wrap.getContext('2d');
     this.tempArray = [];
-    
+
     return this.init();
 }
 Draw.prototype = {
     init: function() {
-        var self=this;//利用一个变量中转指针对象
-        this.wrap.width=screen.width;
-        this.wrap.height=screen.height*0.5;
-        $('html').style.fontSize=screen.height*0.05+'px';
-        this.setWidth(defualtWidth=screen.width*0.005);
+        var self = this; //利用一个变量中转指针对象
+        this.wrap.width = screen.width;
+        this.wrap.height = screen.height * 0.5;
+        $('html').style.fontSize = screen.height * 0.05 + 'px';
+        this.setWidth(screen.width * 0.005);
         //设置必要的默认参数，保证全平台一致
         if (!this.ctx) throw Error("初始化失败！");
-        this.wrap.addEventListener('touchmove', function (e) {
+        this.wrap.addEventListener('touchmove', function(e) {
             e.preventDefault();
             self.drawing(e);
         });
@@ -55,16 +55,53 @@ Draw.prototype = {
         if (width) this.ctx.lineWidth = width;
     },
     setColor: function(color) {
-        if (color) this.ctx.lineColor = color;
+        if (color) this.ctx.strokeStyle = color;
     },
-    clearAll: function () {
-        this.ctx.clearRect(0,0,this.wrap.width,this.wrap.height);
+    clearAll: function() {
+        this.ctx.clearRect(0, 0, this.wrap.width, this.wrap.height);
     }
 }
 
 function init() {
-    var drawIt=new Draw('#paper');
-    $('#clear-btn').addEventListener('click',function () {
+    var drawIt = new Draw('#paper');
+    $('#clear-btn').addEventListener('click', function() {
         drawIt.clearAll();
-    })
+    });
+    $('#tool-bar').addEventListener('click', function(e) {
+        switch (e.target.id) {
+            case 'set-lineWidth':
+                {
+                    $('#width-setter').style.display = 'block';
+                    $('#colors').style.display = 'none';
+                    break;
+                }
+            case 'set-lineColor':
+                {
+                    $('#width-setter').style.display = 'none';
+                    $('#colors').style.display = 'block';
+                    break;
+                }
+        }
+    });
+    $('#tool-bar').addEventListener('touchend', function(e) {
+        if (e.target.id == 'width-setter') {
+            var num = e.target.value / 120;
+            drawIt.setWidth((num + 1) * screen.width * 0.005);
+            $('#set-lineWidth').style.border = 0.4 * (1 - num) + "rem solid #eee";
+        } else
+        if (e.target.id.match('color*')) {
+            var color = window.getComputedStyle(e.target).backgroundColor;
+            drawIt.setColor(color);
+            $('#set-lineColor').style.backgroundColor = color;
+        }
+    });
+
+    var colors = ['#FFCD00', '#F5001D', '#2DD700', '#624AD8', '#000000'];
+    for (var i in colors) {
+        var node = document.createElement('button');
+        node.className = 'list-btn color-btn';
+        node.id = 'coloe' + i;
+        node.style.backgroundColor = colors[i];
+        $('#colors').appendChild(node);
+    }
 }
